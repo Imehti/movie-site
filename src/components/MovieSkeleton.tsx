bd16b1f5f3ae5b5047c86c606db1725c6213dfa9
Mahdi <khajavim497@gmail.com>
2024-04-21 10:20:40 +0430

    delete none used imports

--- src/components/MovieSkeleton.tsx
+++ src/components/MovieSkeleton.tsx
@@ -1,14 +1,9 @@
-import { Card, CardBody, Skeleton, SkeletonText, Spinner } from "@chakra-ui/react"
+import { Spinner } from "@chakra-ui/react"
 
 function MovieSkeleton(){
     return(
         <>
-        {/* <Card>
-            <Skeleton />
-            <CardBody>
-                <SkeletonText />
-            </CardBody>
-        </Card> */}
+  
         <Spinner background='white' width='50px'></Spinner>
         </>
     )
